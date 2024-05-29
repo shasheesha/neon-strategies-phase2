1f929d2c120f11d0dce5f786c92071f1a3e13f98
Shasheesha Janith Dissanayake <85898263+shasheesha@users.noreply.github.com>
2024-05-30 01:43:25 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,21 @@
 import About from "@/components/sections/About/page";
 import GetInTouch from "@/components/sections/GetInTouch/page";
 import OurProcess from "@/components/sections/OurProcess/page";
-// import FAQ from "@/components/sections/FAQ/page";
+import FAQ from "@/components/sections/FAQ/page";
 import Welcome from "@/components/sections/Welcome/page";
 import WhatWeDo from "@/components/sections/WhatWeDo/page";
 import AnimatedSection from '../components/Animation/AnimationFadeUp/page';
 import ParallaxBanner from "@/components/sections/ParallexBanner/page";
+import VideoBanner from "@/components/sections/VideoBanner/page";
 
 export default function Home() {
   return (
     <>
     <AnimatedSection>
       <Welcome />
+    </AnimatedSection>
+    <AnimatedSection>
+      <VideoBanner />
     </AnimatedSection>
     <AnimatedSection>
       <WhatWeDo />
