35bee0f95c9a0570cbc720ec0e1086177cd54518
Shasheesha Janith Dissanayake <85898263+shasheesha@users.noreply.github.com>
2024-05-29 21:13:25 +0530

    fixing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "@/styles/globals.scss";
+import HeaderNavigation from "@/layout/HeaderNavigation/page";
+import Footer from "@/layout/Footer/page";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +18,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <HeaderNavigation />
+        
+        {children}
+        <Footer />
+      </body>
     </html>
   );
 }
