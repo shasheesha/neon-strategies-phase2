54924fe5d1395cedaf63ff1278bb2b342065b2a4
Shasheesha Janith Dissanayake <85898263+shasheesha@users.noreply.github.com>
2024-05-29 23:53:35 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "@/styles/globals.scss";
 import HeaderNavigation from "@/layout/HeaderNavigation/page";
-import Footer from "@/layout/Footer/page";
+// import Footer from "@/layout/Footer/page";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,7 +21,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <HeaderNavigation />
         {children}
-        <Footer />
+        {/* <Footer /> */}
       </body>
     </html>
   );
